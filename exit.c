6c5ac6c2b77bb11c79c108f8e9ca160a68f94efb
amywandd <njeriamy2@gmail.com>
2023-05-25 06:06:37 -0700

    simple shell

--- exit.c
+++ exit.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  **_strncpy - copies a string
  *@dest: the destination string to be copied to
@@ -9,27 +8,25 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
-
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	if (i < n)
-	{
-		j = i;
-		while (j < n)
-		{
-			dest[j] = '\0';
-			j++;
-		}
-	}
-	return (s);
+int i, j;
+char *s = dest;
+i = 0;
+while (src[i] != '\0' && i < n - 1)
+{
+dest[i] = src[i];
+i++;
 }
-
+if (i < n)
+{
+j = i;
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+}
+return (s);
+}
 /**
  **_strncat - concatenates two strings
  *@dest: the first string
@@ -39,24 +36,22 @@ char *_strncpy(char *dest, char *src, int n)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
-
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	if (j < n)
-		dest[i] = '\0';
-	return (s);
+int i, j;
+char *s = dest;
+i = 0;
+j = 0;
+while (dest[i] != '\0')
+i++;
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
 }
-
+if (j < n)
+dest[i] = '\0';
+return (s);
+}
 /**
  **_strchr - locates a character in a string
  *@s: the string to be parsed
@@ -65,10 +60,9 @@ char *_strncat(char *dest, char *src, int n)
  */
 char *_strchr(char *s, char c)
 {
-	do {
-		if (*s == c)
-			return (s);
-	} while (*s++ != '\0');
-
-	return (NULL);
+do {
+if (*s == c)
+return (s);
+} while (*s++ != '\0');
+return (NULL);
 }
